8c20ae86e671e905633ca08b0a8fafa33e4efcbc
pmounika269-collab <pmounika269@gmail.com>
2026-01-04 14:45:36 +0530

    Created build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+echo "Day7 ACCESING GIT FROM JENKINS"
+echo "current user"
+whoami
+echo "current directory"
+pwd
+echo "list of files
+ls -l
+echo "Files creating files from github script"
+touch day7_githubscipt.txt
+echo "build completed successfuly"
